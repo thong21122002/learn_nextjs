29e2829edac5574a7746afcc683620566ec0b378
Au Minh Thong <auminhthongpdct@gmail.com>
2025-03-04 16:40:10 +0700

    create header footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>đây là header</header>
+        {children}
+        <footer>đây là footer</footer>
+      </body>
     </html>
   )
 }
